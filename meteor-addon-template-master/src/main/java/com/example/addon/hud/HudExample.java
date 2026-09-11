fd7ca2fccc7e610b99f5179adfa8b15995ed8506
haohocgioi1234-hub <haohocgioi1234@gmail.com>
2026-09-11 19:49:34 +0700

    Update HudExample.java

--- meteor-addon-template-master/src/main/java/com/example/addon/hud/HudExample.java
+++ meteor-addon-template-master/src/main/java/com/example/addon/hud/HudExample.java
@@ -1,6 +1,6 @@
 package com.example.addon.hud;
 
-import com.example.addon.AddonTemplate;
+import com.example.addon.Addon;
 import meteordevelopment.meteorclient.systems.hud.HudElement;
 import meteordevelopment.meteorclient.systems.hud.HudElementInfo;
 import meteordevelopment.meteorclient.systems.hud.HudRenderer;
